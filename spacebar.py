5504318b537cd04b89a00661f001ad199f64b11a
v1pershot <caden.fluker@gmail.com>
2024-05-22 20:14:48 -0500

    spacebar.py

--- spacebar.py
+++ spacebar.py
@@ -3,6 +3,16 @@ import keyboard #pip install keyboard
 
 counter = 0
 
+delay = float(input('How fast would you like to click?>> '))
+print('You have 3 seconds to navigate to your webpage')
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(1)
+print('GO')
+
 while True:
   if keyboard.is_pressed('q'):
     print('Stoping loop')
@@ -11,4 +21,5 @@ while True:
   keyboard.press_and_release('space')
   counter = counter + 1
   print('This is iteration#',counter)
+  time.sleep(delay)
   
