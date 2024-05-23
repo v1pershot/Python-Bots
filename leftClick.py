0ccd0cb826f6656b16e2848e59f65b19963bf595
v1pershot <caden.fluker@gmail.com>
2024-05-22 20:12:22 -0500

    leftClick.py

---
+++ leftClick.py
@@ -0,0 +1,26 @@
+import time
+import keyboard #pip install keyboard
+import pyautogui as pya
+
+counter = 0
+
+delay = float(input('How fast would you like to click?>> '))
+print('You have 3 seconds to navigate to your webpage')
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(1)
+print('GO')
+
+while True:
+  if keyboard.is_pressed('q'):
+    print('Stoping loop')
+    print('Space bar was pressed ' + numbs + ' times') 
+    break
+  pya.click()
+  counter = counter + 1
+  print('This is iteration#',counter)
+  time.sleep(delay)
+  
